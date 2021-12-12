134316b085aee0bdf5a47617378f599f06163673
h141@pc.email.ne.jp <h141@pc.email.ne.jp>
2021-12-12 16:10:23 +0900

    RC-25

--- 00_sns_infra_alert.sh
+++ 00_sns_infra_alert.sh
@@ -3,15 +3,15 @@ PYTHONIOENCODING=UTF-8
 # -------------------------------------
 # SNS Infra Alert
 # -------------------------------------
-sns_infra_alert="infra-alert-snstopic"
-aws sns list-topics | jq -r '.Topics[].TopicArn'|cut --d : --f 6|grep "^${sns_infra_alert}$"
+sns_infra_alert_name="infra-alert-snstopic"
+aws sns list-topics | jq -r '.Topics[].TopicArn'|cut --d : --f 6|grep "^${sns_infra_alert_name}$"
 rtn_sns_infra_alert=$?
 if [ ${rtn_sns_infra_alert} == 0 ]; then
-  echo "SNS topic \"${sns_infra_alert}\" already exists." 
+  echo "SNS topic \"${sns_infra_alert_name}\" already exists." 
 else
   echo Create SNS Topic and Subscription
   echo
-  read -p "Enter mail address for SNStopic \"${sns_infra_alert}\":" mailaddress
+  read -p "Enter mail address for SNStopic \"${sns_infra_alert_name}\":" mailaddress
   # ------------------------------------------
   template_file="file://./00_sns_infra_alert.yaml"
   stackname="stack-tmp-infra-alert-snstopic"
@@ -22,6 +22,24 @@ else
     --tags Key=Product,Value=IntegratedNotification \
     --parameters \
     "ParameterKey=vMailAddress,ParameterValue='${mailaddress}'" \
-    "ParameterKey=vSNSTopic,ParameterValue='${sns_infra_alert}'"
+    "ParameterKey=vSNSTopic,ParameterValue='${sns_infra_alert_name}'"
+  # -------------------------------------
+  # wait status change
+  status="CREATE_IN_PROGRESS"
+  echo Start CloudFormation Stack
+  while [ $status == "CREATE_IN_PROGRESS" ]; do
+    echo sleep 30
+    sleep 30
+    status=$(aws cloudformation describe-stacks --stack-name $stackname | jq -r .Stacks[].StackStatus)
+    echo Now CloudFormation Stack Status $status
+  done
+  if [ $status != "CREATE_COMPLETE" ]; then
+    echo "ERROR  CloudFormation Stack Status $status"
+    printf '\033[31m%s\033[m\n' "Forced Termination"
+    exit 
+  fi
   { set +eu; } 2>/dev/null
 fi
+sns_infra_alert=$( aws sns list-topics | jq -r '.Topics[].TopicArn'| \
+  grep ":${sns_infra_alert_name}$" )
+
