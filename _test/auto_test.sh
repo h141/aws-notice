873673fa2a3d1b61d24b4765567fd4914aae0f6d
h141@pc.email.ne.jp <h141@pc.email.ne.jp>
2021-12-16 07:55:05 +0900

    RC-39

--- _test/auto_test.sh
+++ _test/auto_test.sh
@@ -1,4 +1,4 @@
 test01="aws lambda invoke --function-name shub-siem2securityhub-func"
 cd $(dirname $0)
 json=$(cat securityhub_01.json)
-test01 --payload "'"${json}"'"
+${test01} --payload "'"${json}"'"
