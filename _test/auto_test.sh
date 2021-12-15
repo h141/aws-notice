44216d28ea2cb85d23156d33503d8ea048b67122
h141@pc.email.ne.jp <h141@pc.email.ne.jp>
2021-12-16 08:08:26 +0900

    RC-41

--- _test/auto_test.sh
+++ _test/auto_test.sh
@@ -1,3 +1,9 @@
+BASEDIR="$HOME/aws-notice"
+cd "$BASEDIR" || exit
+git fetch >/dev/null 2>&1
+git checkout main >/dev/null  2>&1
+git pull --rebase >/dev/null 2>&1
+# --
 test01="aws lambda invoke --function-name shub-siem2securityhub-func"
 cd $(dirname $0)
 json=$(cat securityhub_01.json|tr -d '\r'|tr -d '\n')
