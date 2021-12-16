851879ca6bfbee710cd17f23985adba2dd885e7c
h141@pc.email.ne.jp <h141@pc.email.ne.jp>
2021-12-16 09:04:56 +0900

    RC-43

--- _test/auto_test.sh
+++ _test/auto_test.sh
@@ -6,6 +6,13 @@ git checkout main >/dev/null  2>&1
 git pull --rebase >/dev/null 2>&1
 # --
 test01="aws lambda invoke --function-name shub-siem2securityhub-func"
+
+testname="siem_01"
 cd ${TESTDIR}
-json=$(cat securityhub_01.json|tr -d '\r'|tr -d '\n')
-${test01} --payload "'"${json}"'"
+json=$(cat ${testname}.json|tr -d '\r'|tr -d '\n')
+echo ${test01} --payload "${json}" "$HOME/test_${testname}.log"
+
+testname="siem_02"
+cd ${TESTDIR}
+json=$(cat ${testname}.json|tr -d '\r'|tr -d '\n')
+echo ${test01} --payload "${json}" "$HOME/test_${testname}.log"
