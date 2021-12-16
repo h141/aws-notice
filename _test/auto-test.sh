1b124aed9757a08a2b1f0b9d23749bb5c4f08b1c
h141@pc.email.ne.jp <h141@pc.email.ne.jp>
2021-12-16 13:04:38 +0900

    RC-48

--- _test/auto-test.sh
+++ _test/auto-test.sh
@@ -3,7 +3,7 @@ cd ${TESTDIR}
 # --
 test01="aws lambda invoke --function-name shub-siem2securityhub-func"
 cd ${TESTDIR}
-
+AWSID=$(aws sts get-caller-identity|jq -r .Account)
 tmpfile=$(mktemp)
 echo siem test
 for testfile in siem_*.json ;do
@@ -11,10 +11,11 @@ for testfile in siem_*.json ;do
   echo -input json------------------------
   cat ${testfile}
   echo -cmd------------------------
-  json=$(cat ${testfile}|base64|tr -d "\n")
+  json=$(cat ${testfile}|sed 's/${AWSID}/'${AWSID}'/g'|base64|tr -d "\n")
   ${test01} --payload "${json}" "${tmpfile}"
   echo -outfile------------------------
   cat "${tmpfile}"
+  echo
   echo -------------------------
   read -p "next test?" tmp
 done
