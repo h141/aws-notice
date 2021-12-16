70dbd53bf8cccbcd31828407407ff40a909fa2b0
h141@pc.email.ne.jp <h141@pc.email.ne.jp>
2021-12-16 16:09:52 +0900

    RC-51

--- _test/auto-test.sh
+++ _test/auto-test.sh
@@ -3,15 +3,17 @@ cd ${TESTDIR}
 # --
 test01="aws lambda invoke --function-name shub-siem2securityhub-func"
 cd ${TESTDIR}
+SNSNAME=$(aws ssm get-parameter --name SNS_default | jq -r .Parameter.Value)
 AWSID=$(aws sts get-caller-identity|jq -r .Account)
 tmpfile=$(mktemp)
 echo siem test
 for testfile in siem_*.json ;do
+  json=$(cat ${testfile}|sed 's/${AWSID}/'${AWSID}'/g'|sed 's/${SNSNAME}/'${SNSNAME}'/g')
   echo test ${testfile}
   echo -input json------------------------
-  cat ${testfile}|sed 's/${AWSID}/'${AWSID}'/g'
+  echo ${json}
   echo -cmd------------------------
-  json=$(cat ${testfile}|sed 's/${AWSID}/'${AWSID}'/g'|base64|tr -d "\n")
+  json64=$(echo ${json}|base64|tr -d "\n")
   ${test01} --payload "${json}" "${tmpfile}"
   echo -outfile------------------------
   cat "${tmpfile}"
