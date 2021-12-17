c2acd9fbcc0eced997fe06c4e78a375d172f88b2
h141@pc.email.ne.jp <h141@pc.email.ne.jp>
2021-12-17 09:58:25 +0900

    RC-57

--- _test/auto-test.sh
+++ _test/auto-test.sh
@@ -1,26 +1,39 @@
 TESTDIR=$(cd $(dirname $0); pwd)
 cd ${TESTDIR}
-# --
-test01="aws lambda invoke --function-name shub-siem2securityhub-func"
-cd ${TESTDIR}
+# =====================================================
+TMPFILE=$(mktemp)
 SNSNAME=$(aws ssm get-parameter --name SNS_default | jq -r .Parameter.Value)
 AWSID=$(aws sts get-caller-identity|jq -r .Account)
-tmpfile=$(mktemp)
 # =====================================================
-echo siem test
-for testfile in siem_*.json ;do
+# --
+function test_lambda_func () {
+  funcname=$1
+  filepath=$2
+  json=$(eval echo $(cat ${filepath}| sed 's/\\/\\\\/g'| sed 's/"/\\"/g'| tr -d "\n"))
+  echo ========================================
+  echo test ${funcname} ${filepath}
   date=$(TZ=JST-9 date)
-  json=$(eval echo $(cat ${testfile}| sed 's/\\/\\\\/g'| sed 's/"/\\"/g'| tr -d "\n"))
-  echo test ${testfile}
   echo DATE: ${date}
   echo -input json------------------------
   echo ${json}
   echo -cmd------------------------
   json64=$(echo ${json}|base64|tr -d "\n")
-  ${test01} --payload "${json64}" "${tmpfile}"
+  aws lambda invoke --function-name ${funcname} --payload "${json64}" "${TMPFILE}"
   echo -outfile------------------------
-  cat "${tmpfile}"
+  cat "${TMPFILE}"
   echo
   echo ========================================
+}
+# --
+echo 1: $1
+echo 2: $2
+echo 3: $3
+
+exit
+
+
+cd ${TESTDIR}
+for testfile in siem_*.json ;do
+  test_lambda_func "shub-siem2securityhub-func" "${testfile}"
   read -p "next test?" tmp
 done
