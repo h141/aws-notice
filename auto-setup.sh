3b20680699c5810957475a0054ada16ad0dc59db
h141@pc.email.ne.jp <h141@pc.email.ne.jp>
2021-12-17 10:04:16 +0900

    RC-59

--- auto-setup.sh
+++ auto-setup.sh
@@ -20,7 +20,8 @@ else
 fi
 # ------------
 if [ "_${opt1}" == "_--test" ]; then
-  sh "${BASEDIR}/_test/auto-test.sh" $@
+  echo test "$@"
+  sh "${BASEDIR}/_test/auto-test.sh" "$@"
   exit
 fi
 # ------------
