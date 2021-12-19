22f0d48ada25663b763f589e4bbfd9ea0be68a0a
h141@pc.email.ne.jp <h141@pc.email.ne.jp>
2021-12-20 08:49:21 +0900

    RC-72

--- _test/auto-test.sh
+++ _test/auto-test.sh
@@ -1,4 +1,5 @@
 TESTDIR=$(cd $(dirname $0); pwd)
+# =====================================================
 cd ${TESTDIR}
 # =====================================================
 TMPFILE=$(mktemp)
@@ -38,52 +39,61 @@ declare -A prefixs=(
   ["board"]="securityhub"
   ["connect"]="securityhub"
 )
+function test_type_lambda_funcs () {
+  tmp_type=$1
+  func_name="${funcs[$tmp_type]}"
+  prefix_name="${prefixs[$tmp_type]}"
+  for testfile in ${prefix_name}_*.json ;do
+    echo "testfile ${testfile}"
+    echo "-----"
+    test_lambda_func "${func_name}" "${testfile}"
+    read -p "next test?" tmp
+  done
+  org_env_file="org_env_${func_name}.json"
+  aws lambda get-function-configuration --function-name "${func_name}" | \
+    jq -r .Environment >"${org_env_file}"
+  testfile=${prefix_name}_02.json
+  for envtestfile in env_${tmp_type}_*.json ;do
+    echo "update environment ${envtestfile}"
+    echo "-----"
+    cat ${envtestfile}
+    echo
+    echo "-----"
+    aws lambda update-function-configuration --function-name "${func_name}" \
+      --environment "$(cat ${envtestfile})" >/dev/null
+    test_lambda_func "${func_name}" "${testfile}"
+    read -p "next test?" tmp
+    echo ========================================
+  done
+  echo "update environment ${org_env_file}"
+  echo "-----"
+  aws lambda update-function-configuration --function-name "${func_name}" \
+    --environment "$(cat ${org_env_file})"
+  echo "-----"
+  aws lambda get-function-configuration --function-name "${func_name}"
+  read -p "next test?" tmp
+}
 # --
 cd ${TESTDIR}
 echo ========================================
+prfix_ssm=_ssm
+for ssmfile in ${prfix_ssm}_*.txt ;do
+  ssm_name=$(echo ${ssmfile} | sed "s/${prfix_ssm}_//g" | sed "s/.txt//g")
+  ssm_value="$(cat ${ssmfile})"
+  echo create ${ssm_name}
+  aws ssm put-parameter --name "${ssm_name}" --value "${ssm_value}"
+done
+exit
+echo ========================================
 if [ "_$test_type" == "_" -o "$test_type" == "all" ];then
   for tmp_type in siem sns board connect ;do
-    func_name="${funcs[$tmp_type]}"
-    prefix_name="${prefixs[$tmp_type]}"
-    for testfile in ${prefix_name}_*.json ;do
-      echo "testfile ${testfile}"
-      echo "-----"
-      # test_lambda_func "${func_name}" "${testfile}"
-      # read -p "next test?" tmp
-    done
-    org_env_file="org_${func_name}.json"
-    aws lambda get-function-configuration --function-name "${func_name}" | \
-      jq -r .Environment >"${org_env_file}"
-    testfile=${prefix_name}_02.json
-    for envtestfile in env_${prefix_name}_*.json ;do
-      echo "update environment ${envtestfile}"
-      echo "-----"
-      cat ${envtestfile}
-      echo
-      echo "-----"
-      aws lambda update-function-configuration --function-name "${func_name}" \
-        --environment "$(cat ${envtestfile})" >/dev/null
-      test_lambda_func "${func_name}" "${testfile}"
-      read -p "next test?" tmp
-      echo ========================================
-    done
-    echo "update environment ${org_env_file}"
-    echo "-----"
-    aws lambda update-function-configuration --function-name "${func_name}" \
-      --environment "$(cat ${org_env_file})"
-    echo "-----"
-    aws lambda get-function-configuration --function-name "${func_name}"
-    read -p "next test?" tmp
-    echo ========================================
+    test_type_lambda_funcs ${tmp_type}
   done
 else
   func_name="${funcs[$test_type]}"
   prefix_name="${prefixs[$test_type]}"
   if [ "_$test_no" == "_" -a "_$func_name" != "_" -a "_$prefix_name" != "_" ];then
-    for testfile in ${prefix_name}_*.json ;do
-      test_lambda_func "${func_name}" "${testfile}"
-      read -p "next test?" tmp
-    done
+    test_type_lambda_funcs ${test_type}
   elif [ "_$test_no" != "_" -a "_$func_name" != "_" -a "_$prefix_name" != "_" ];then
     test_lambda_func "${func_name}" "${prefix_name}_${test_no}.json"
     read -p "next test?" tmp
